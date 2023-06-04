c9703d51720812bc273f17109bff7d213bd90649
leekli <li.kirkham@live.com>
2023-06-04 15:49:13 +0100

    updates port for listen file using process env

--- listen.js
+++ listen.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: `${__dirname}/.env.${process.env.NODE_ENV}` });
 require("./connection.js");
 
 // Server connection and listener
-const PORT = 3000;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, (err) => {
   if (err) throw err;
